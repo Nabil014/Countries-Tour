2044385046c3b9656e43b1e992cbb5d5d5f5e189
Nabil014 <nallis014@gmail.com>
2023-09-26 20:05:19 -0300

    clear db

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const server = require('./src/app.js')
 const { conn } = require('./src/db.js')
 const loadDB = require('./src/DBload/DBload.js')
 
-conn.sync({ force: false }).then(async () => {
+conn.sync({ force: true }).then(async () => {
   try {
     await loadDB()
     server.listen(process.env.DB_PORT, () => {
