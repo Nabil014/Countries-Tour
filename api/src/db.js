7f710df144ee973d63580e4af867decbe83d1f06
Nabil014 <nallis014@gmail.com>
2023-09-26 20:05:06 -0300

    clear db

--- api/src/db.js
+++ api/src/db.js
@@ -5,12 +5,13 @@ const path = require('path')
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT } = process.env
 
 // const sequelize = new Sequelize(
-//   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`,
+//   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries-tour`,
 //   {
 //     logging: false, // set to console.log to see the raw SQL queries
-//     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+//     native: false,
 //   }
 // )
+
 let sequelize =
   process.env.NODE_ENV === 'production'
     ? new Sequelize({
